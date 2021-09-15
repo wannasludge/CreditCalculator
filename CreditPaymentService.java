47749ad5a5bf922b888cd3d10cd5cb54f550abd9
Alexander mokhov <wannasludge@yandex.ru>
2021-09-15 22:45:26 +0300

    Code and .gitignore

---
+++ CreditPaymentService.java
@@ -0,0 +1,10 @@
+public class CreditPaymentService {
+    public double calculate(double summ, double period){
+        double creditRateYear = 0.0999;
+        double creditRateMounth = creditRateYear / 12;
+        double coef = (creditRateMounth * (Math.pow((1 + creditRateMounth), period))) / (Math.pow((1 + creditRateMounth), period) - 1);
+        double payment = coef * summ;
+        return payment;
+    }
+}
+
