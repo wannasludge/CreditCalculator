dff89894132739b910d89e8a27c3fbb71580ab32
Alexander mokhov <wannasludge@yandex.ru>
2021-09-18 13:17:21 +0300

    Update code

--- CreditPaymentService.java
+++ CreditPaymentService.java
@@ -1,5 +1,5 @@
 public class CreditPaymentService {
-    public double calculate(double summ, double period){
+    public double calculate(double summ, double period) {
         double creditRateYear = 0.0999;
         double creditRateMounth = creditRateYear / 12;
         double coef = (creditRateMounth * (Math.pow((1 + creditRateMounth), period))) / (Math.pow((1 + creditRateMounth), period) - 1);
--- Main.java
+++ Main.java
@@ -2,13 +2,13 @@ public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
         double payment = service.calculate(1_000_000, 12);
-        System.out.printf("%.0f",payment);
+        System.out.printf("%.0f", payment);
         System.out.println(" ");
         payment = service.calculate(1_000_000, 24);
-        System.out.printf("%.0f",payment);
+        System.out.printf("%.0f", payment);
         System.out.println(" ");
         payment = service.calculate(1_000_000, 36);
-        System.out.printf("%.0f",payment);
+        System.out.printf("%.0f", payment);
         System.out.println(" ");
     }
 }
